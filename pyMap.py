83e6fa98f7edae84312d608596ad00f07582c086
Dayu <yanfei.yu@bayesba.com>
2018-04-09 14:16:23 +0800

    Update for utils.

--- pyMap.py
+++ pyMap.py
@@ -10,13 +10,13 @@ from PIL import Image
 from tqdm import trange
 
 URL = {
-    "gaode": "http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}",
-    "gaode.image": "http://webst02.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}",
-    "tianditu": "http://t2.tianditu.cn/DataServer?T=vec_w&X={x}&Y={y}&L={z}",
-    "googlesat": "http://khm0.googleapis.com/kh?v=203&hl=zh-CN&&x={x}&y={y}&z={z}",
-    "tianditusat":"http://t2.tianditu.cn/DataServer?T=img_w&X={x}&Y={y}&L={z}",
-    "esrisat":"http://server.arcgisonline.com/arcgis/rest/services/world_imagery/mapserver/tile/{z}/{y}/{x}",
-    "gaode.road": "http://webst02.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8"
+    "gaode.sat": "http://webst02.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}",#
+    "tianditu.sat": "http://t2.tianditu.cn/DataServer?T=vec_w&X={x}&Y={y}&L={z}", ##
+    "google.sat": "http://khm0.googleapis.com/kh?v=203&hl=zh-CN&&x={x}&y={y}&z={z}",#
+    "esri.sat":"http://server.arcgisonline.com/arcgis/rest/services/world_imagery/mapserver/tile/{z}/{y}/{x}", #
+    "gaode.road": "http://webst02.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8",#
+    "gaode.stand": "http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}",#
+    "google.stand": "http://mt0.google.cn/vt/lyrs=m@167000000&hl=zh-CN&gl=cn&x={x}&y={y}&z={z}"
 }
 
 
